e282d02911781eabfad0130cdb9243fa854ae49a
Fares Kerkeni <kerkenif11@gmail.com>
2023-06-20 00:10:35 +0200

    profil

--- barber_plus/app/page.tsx
+++ barber_plus/app/page.tsx
@@ -1,10 +1,28 @@
 import { FunctionComponent } from "react";
 import style from "./page.module.scss";
 import Layout from "./layout";
+import Image from "next/image";
 const Home: FunctionComponent = () => {
   return (
     <Layout>
-      <div>je suis dans la home </div>
+      <div className={style.container}>
+        <div className={style.container_profil}>
+          <div className={style.profil}>
+            <div className={style.image}>
+              <Image
+                src="/images/profil.png"
+                width={96}
+                height={96}
+                alt="image de profil"
+              />
+            </div>
+            <div className={style.name}>
+              <p>@BarberPlus</p>
+            </div>
+            <div className={style.icons}></div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
