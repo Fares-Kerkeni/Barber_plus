5280c1ced825b0104a781d605e01fc7b1a8fe6d9
Fares Kerkeni <kerkenif11@gmail.com>
2023-06-19 22:18:40 +0200

    initialisation du projet

---
+++ cline_next_app/app/page.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from "react";
+import style from "./page.module.scss";
+import Layout from "./layout";
+const Home: FunctionComponent = () => {
+  return (
+    <Layout>
+      <div>je suis dans la home </div>
+    </Layout>
+  );
+};
+
+export default Home;
