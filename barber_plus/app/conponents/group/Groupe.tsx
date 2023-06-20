aad45e91f06333057fd9208aa1364d0cd615bdba
Fares Kerkeni <kerkenif11@gmail.com>
2023-06-20 02:11:07 +0200

    creation des groupe

---
+++ barber_plus/app/conponents/group/Groupe.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent } from "react";
+import styles from "./groupe.module.scss";
+import Image from "next/image";
+
+interface GroupeProps {
+  imageSrc: string;
+  socialMediaLink: string;
+  text: string;
+}
+
+const Groupe: FunctionComponent<GroupeProps> = ({
+  imageSrc,
+  socialMediaLink,
+  text,
+}) => {
+  return (
+    <a href={socialMediaLink} className={styles.container}>
+      <div className={styles.logo}>
+        <Image src={imageSrc} width={60} height={60} alt="logo" />
+      </div>
+      <div className={styles.description}>
+        <p>{text}</p>
+      </div>
+      <div></div>
+    </a>
+  );
+};
+
+export default Groupe;
--- barber_plus/app/page.tsx
+++ barber_plus/app/page.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from "react";
 import style from "./page.module.scss";
 import Layout from "./layout";
 import Image from "next/image";
+import Groupe from "./conponents/group/Groupe";
 const Home: FunctionComponent = () => {
   return (
     <Layout>
@@ -21,6 +22,33 @@ const Home: FunctionComponent = () => {
             </div>
             <div className={style.icons}></div>
           </div>
+        </div>
+        <div className={style.container_groupe}>
+          <Groupe
+            imageSrc="/images/google.png"
+            socialMediaLink="https://lien-de-mon-reseau-social.com"
+            text="Illuminez Google avec vos étoiles et partagez votre expérience !"
+          />
+          <Groupe
+            imageSrc="/images/snap.png"
+            socialMediaLink="https://lien-de-mon-reseau-social.com"
+            text="Venez explorer la magie de Snapchat dès maintenant !"
+          />
+          <Groupe
+            imageSrc="/images/instagram.png"
+            socialMediaLink="https://lien-de-mon-reseau-social.com"
+            text="Immergez-vous dans l'univers captivant d'Instagram, rejoignez-nous aujourd'hui !"
+          />
+          <Groupe
+            imageSrc="/images/tik-tok.png"
+            socialMediaLink="https://lien-de-mon-reseau-social.com"
+            text="Plongez dans l'énergie contagieuse de TikTok, rejoignez-nous maintenant !"
+          />
+          <Groupe
+            imageSrc="/images/facebook.png"
+            socialMediaLink="https://lien-de-mon-reseau-social.com"
+            text="Découvrez la communauté vibrante de Facebook, rejoignez-nous dès maintenant !"
+          />
         </div>
       </div>
     </Layout>
