948c6463ff6e14fb62cc43b5ed19a401b06d5c42
Fares Kerkeni <kerkenif11@gmail.com>
2023-06-22 23:31:10 +0200

    eslint

--- barber_plus/app/layout.tsx
+++ barber_plus/app/layout.tsx
@@ -2,10 +2,6 @@ import "./reset.scss";
 
 import style from "./layout.module.scss";
 import Nav from "./conponents/Nav/nav";
-export const metadata = {
-  title: "Barber Plus",
-  description: "C'est un site de coiffure pour homme ",
-};
 
 export default function RootLayout({
   children,
